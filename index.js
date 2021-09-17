a6d3dad56bb4f4c635ceea2a4644f2149a20fe70
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-09-17 11:59:55 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let number_one=12;
+let number_two=12;
+let sum=number_one+number_two;
+alert(sum);
+
+let string='hello'
+let sum1=number_one+ " " +string;
+alert(sum1)
+
+let string_one="hello"
+let string_two="world"
+alert(string_one+string_two)
